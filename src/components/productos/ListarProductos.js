2272a5d3211b2584752d09c625959220288e65d6
Emilse <emilsearias@gmail.com>
2021-04-19 10:28:39 -0300

    se agrego el sistema de rutas en app

---
+++ src/components/productos/ListarProductos.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ListarProductos = () => {
+    return (
+        <div>
+            <h1>Pagina lista de productos</h1>
+        </div>
+    );
+};
+
+export default ListarProductos;<h1>Pagina lista de productos</h1>
